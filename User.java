02d5f47c920598c8162b29fd0f3309fdd1e182b4
Valeria Melnyk <valeria1melnyk@gmail.com>
2025-03-13 20:59:09 +0200

    add class for users

---
+++ User.java
@@ -0,0 +1,25 @@
+public class User {
+    public int id;
+    public String name;
+    public int age;
+    public String email;
+    public User(int id, String name, int age, String email) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public String getEmail() {
+        return email;
+    }
+}

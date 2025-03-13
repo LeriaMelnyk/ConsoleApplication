b3de6a80c0f06163687365b30133b6ba3934744d
Valeria Melnyk <valeria1melnyk@gmail.com>
2025-03-13 22:22:35 +0200

    add class for users with CRUD operations

---
+++ UserService.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserService {
+    private final List<User> users=new ArrayList<>();
+    private int userIdCounter=0;
+
+    public void addUser(String name, String email, int age){
+        users.add(new User(userIdCounter++, name, age, email));
+    }
+
+    public void deleteUser(int id) {
+        users.removeIf(user -> user.getId() == id);
+    }
+
+    public boolean updateUser(int id, String newName,int newAge, String newEmail) {
+        for (int i = 0; i < users.size(); i++) {
+            if (users.get(i).getId() == id) {
+                User updatedUser = new User(id, newName, newAge, newEmail);
+                users.set(i, updatedUser);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public List<User> listUsers() {
+        return users;
+    }
+}

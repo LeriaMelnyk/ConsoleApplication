0dd400fe39648a0e16d35197106bd6ff2ef50dcc
Valeria Melnyk <valeria1melnyk@gmail.com>
2025-03-13 21:33:15 +0200

    add changes

--- User.java
+++ User.java
@@ -1,8 +1,8 @@
 public class User {
-    public int id;
-    public String name;
-    public int age;
-    public String email;
+    public final int id;
+    public final String name;
+    public final int age;
+    public final String email;
     public User(int id, String name, int age, String email) {
         this.id = id;
         this.name = name;
@@ -21,5 +21,10 @@ public class User {
     }
     public String getEmail() {
         return email;
+    }
+
+    @Override
+    public String toString() {
+        return "User: id="+id+", name="+name+", age="+age+", email="+email;
     }
 }
